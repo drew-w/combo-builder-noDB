7001c1fbac3eacb669bb6e83d21afe4f935a2956
Drew Woodmansee <eesnamdoow@gmail.com>
2021-02-03 09:45:20 -0700

    Revert "app working"
    This reverts commit ff7dbf39b9ad25d48fe8b8dd8a902f0bc44458d2.

--- src/Components/Saved.js
+++ src/Components/Saved.js
@@ -2,58 +2,60 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Saved extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedCombos: [],
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            savedCombos: []
+        }
+    }
 
-  componentDidMount() {
-    this.getSaved();
-  }
+    componentDidUpdate() {
+        this.getSaved();
+    }
 
-  getSaved = () => {
-    axios.get("/api/combos").then((res) => {
-      this.setState({ savedCombos: res.data });
-    });
-  };
+    componentDidMount(){
+        this.getSaved();
+    }
 
-  deleteCombo = (id) => {
-    axios.delete(`/api/combos/${id}`).then((res) => {
-      this.setState({
-        savedCombos: res.data,
-      });
-    });
-  };
+    getSaved = () => {
+        axios.get('/api/combos')
+            .then(res => {
+                this.setState({ savedCombos: res.data })
+            })
+    }
 
-  render() {
-    let mappedSaved = this.state.savedCombos.map((e) => {
-      return (
-        <div key={e.id}>
-          <h1>{e.name}</h1>
-          {this.props.editing === "false" ? (
-            <button
-              className="savedButton"
-              onClick={() => this.props.editCombo(e.id)}
-            >
-              Edit
-            </button>
-          ) : (
-            <div></div>
-          )}
-          <button
-            className="savedButton"
-            onClick={() => this.deleteCombo(e.id)}
-          >
-            X
-          </button>
-        </div>
-      );
-    });
+    deleteCombo = (id) => {
 
-    return <div className="Saved">{mappedSaved}</div>;
-  }
+        axios.delete(`/api/combos/${id}`)
+            .then(res => {
+                this.setState({
+                    savedCombos: res.data
+                })
+            })
+    }
+
+    render() {
+        let mappedSaved = this.state.savedCombos.map(e => {
+            return (
+                <div key={e.id}>
+                    <h1>{e.name}</h1>
+                    {this.props.editing === 'false' ?
+                    <button className='savedButton' onClick={() => this.props.editCombo(e.id)}>Edit</button>
+                    :
+                    <div></div>
+
+            }
+                    <button className='savedButton' onClick={() => this.deleteCombo(e.id)}>X</button>
+                </div>
+            )
+        })
+
+        return (
+            <div className='Saved'>
+                {mappedSaved}
+            </div>
+        )
+    }
 }
 
 export default Saved;
